f04f20cff5cd4c96e2bc4c59d6ec2bc42c11398e
Dinith Pamunuwatta <155564826+dinithanki@users.noreply.github.com>
2026-04-21 17:48:43 +0530

    feat(profile): add updateProfile route to userRoutes for profile updates

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/userController.js";
+import {
+  signup,
+  login,
+  logout,
+  updateProfile,
+} from "../controllers/userController.js";
 import { upload } from "../middlewares/uploadMiddleware.js";
-import { uploadProfilePic } from "../controllers/userController.js";
 import { protect, adminOnly } from "../middlewares/authMiddleware.js";
 import { checkAuth } from "../controllers/userController.js";
 import { googleLogin } from "../controllers/userController.js";
@@ -18,11 +22,12 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/upload-profile-pic", upload.single("image"), uploadProfilePic);
 router.post("/logout", logout);
 router.post("/google-login", googleLogin);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
+
+router.put("/update-profile", protect, upload.single("image"), updateProfile);
 
 router.get("/", protect, adminOnly, getAllUsers);
 router.delete("/:id", protect, adminOnly, deleteUser);
